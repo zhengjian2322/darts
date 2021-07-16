2dbf899ba2d4923f0c1b7e1acc6da2087d16e86d
zhengjian <2322171400@qq.com>
2021-07-16 18:09:39 +0800

    formatter

--- aging_evolution.py
+++ aging_evolution.py
@@ -1,5 +1,4 @@
 import random
-from copy import deepcopy
 
 from ConfigSpace.util import get_one_exchange_neighbourhood
 import numpy as np
@@ -7,16 +6,16 @@ from ConfigSpace import ConfigurationSpace, CategoricalHyperparameter
 
 
 class AgingEvolution(object):
-    def __init__(self, cs, cross_rate, mutation_rate, object_function, pop_size=100,
+    def __init__(self, pop, cross_rate, mutation_rate, object_function, pop_size=100,
                  aging_sample_num=10):
         self.cross_rate = cross_rate
         self.mutate_rate = mutation_rate
         self.pop_size = pop_size
         self.object_function = object_function
-        self.pop = np.array(cs.sample_configuration(self.pop_size))
+        self.pop = np.array(pop.sample_configuration(self.pop_size))
         self.aging_sample_num = aging_sample_num
         self.history = np.zeros(self.pop_size)
-        self.fitness(range(self.pop_size),2)
+        self.fitness(range(self.pop_size), 4)
 
     def mutate(self, child):
         neighbourhoods = get_one_exchange_neighbourhood(child, seed=0)
@@ -32,7 +31,8 @@ class AgingEvolution(object):
 
     def fitness(self, candidate_idx, epochs):
         for idx in candidate_idx:
-            self.history[idx] = random.random()#self.object_function(self.pop[idx],epochs)
+            print('----idx', idx)
+            self.history[idx] = self.object_function(self.pop[idx], epochs)
 
     def select(self):
         sample_population = np.random.choice(np.arange(self.pop_size), self.aging_sample_num)
@@ -46,13 +46,12 @@ class AgingEvolution(object):
     def evolve(self):
         parent_idx = self.select()
         child = self.mutate(self.pop[parent_idx])
-        self.pop = np.append(self.pop,[child], axis=0)
+        self.pop = np.append(self.pop, [child], axis=0)
+        with open('results.txt', 'a') as f:
+            f.write(str(np.max(self.history)) + ' ' + ' '.join([str(h) for h in self.history]) + '\n')
         self.fitness([-1], 4)
         self.pop = self.pop[1:]
-        print('self.history',self.history)
 
     def return_best_config(self):
         best_individual = np.where(self.history == np.max(self.history))
         return self.pop[best_individual][0]
-
-
